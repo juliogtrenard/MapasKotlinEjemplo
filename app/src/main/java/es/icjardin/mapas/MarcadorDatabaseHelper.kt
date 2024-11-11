aa7c9ac4d6e578691359ec13cc7732aceb1c0fb2
Julio González <juliogtrenard@gmail.com>
2024-11-11 19:48:07 +0100

    UPDATE clase DatabaseHelper

--- app/src/main/java/es/icjardin/mapas/MarcadorDatabaseHelper.kt
+++ app/src/main/java/es/icjardin/mapas/MarcadorDatabaseHelper.kt
@@ -6,171 +6,151 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 /**
+ * Marcador geográfico con sus atributos.
+ *
+ * @property id ID del marcador.
+ * @property latitud Latitud del marcador.
+ * @property longitud Longitud del marcador.
+ * @property titulo Título del marcador.
+ * @property descripcion Descripción del marcador.
+ */
+data class Marcador(
+    val id: Long,
+    val latitud: Double,
+    val longitud: Double,
+    val titulo: String,
+    val descripcion: String
+)
+
+/**
  * Clase de gestión de la base de datos
  */
-
 class MarcadorDatabaseHelper (context: Context) : SQLiteOpenHelper(
     context, DATABASE_NAME, null, DATABASE_VERSION
 ) {
     companion object {
-        private const val   DATABASE_NAME = "marcadores.db"
-        private const val   DATABASE_VERSION = 1
-        private const val   TABLE_NAME = "marcadores"
+        private const val DATABASE_NAME = "marcadores.db"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_MAP = "Marcadores"
         // y ahora las columnas o atributos
-        private const val   COLUMN_ID = "id"
-        private const val   COLUMN_NOMBRE = "nombre"
-        private const val   COLUMN_LATITUD = "latitud"
-        private const val   COLUMN_LONGITUD = "longitud"
-
+        private const val COLUMN_ID = "Id"
+        private const val COLUMN_TITULO="Ubicacion"
+        private const val COLUMN_LATITUDE="Latitud"
+        private const val COLUMN_LONGITUDE="Longitud"
+        private const val COLUMN_DESCRIPTION="Descripcion"
     }
+
+    /**
+     * Lista de ciudades para insertar en la base de datos.
+     */
+    public val ciudades: List<Marcador> = listOf(
+        Marcador(0,42.849998, -2.683333,"Vitoria-Gasteiz","Capital del País Vasco"),
+        Marcador(0,40.416775, -3.703790,"Madrid","Capital de España"),
+        Marcador(0,41.3784, 2.1925,"Barcelona","Ciudad portuaria en la costa noreste de España"),
+        Marcador(0,39.469907, -0.376288,"Valencia","Conocida por su Ciudad de las Artes y las Ciencias, sus playas y como cuna de la paella."),
+        Marcador(0,37.388630, -5.982329,"Sevilla","La capital de Andalucía."),
+        Marcador(0,37.177336, -3.598557,"Granada","Ciudad histórica conocida por la Alhambra."),
+        Marcador(0,43.263012, -2.934685,"Bilbao","En el País Vasco, es conocida por el Museo Guggenheim."),
+        Marcador(0,40.963374, -5.663540,"Salamanca","Famosa por su Universidad, una de las más antiguas de Europa."),
+        Marcador(0,41.648823, -0.889207,"Zaragoza"," Capital de Aragón, conocida por la Basílica del Pilar."),
+        Marcador(0,36.721276, -4.421321,"Malaga","Ciudad en la Costa del Sol, famosa por sus playas, el Museo Picasso.")
+    )
 
     /**
      * Creadora de la tabla
      */
-    override fun onCreate(db: SQLiteDatabase?) {
-        val createTableQuery =
-            "CREATE TABLE $TABLE_NAME (" +
-                    "$COLUMN_ID INTEGER PRIMARY KEY, " +
-                    "$COLUMN_NOMBRE TEXT, " +
-                    "$COLUMN_LATITUD REAL, " +
-                    "$COLUMN_LONGITUD REAL)"
-        db?.execSQL(createTableQuery)
+    override fun onCreate(db: SQLiteDatabase) {
+        val createTable = "CREATE TABLE $TABLE_MAP (" +
+                "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                " $COLUMN_TITULO TEXT," +
+                "$COLUMN_LATITUDE DOUBLE," +
+                "$COLUMN_LONGITUDE DOUBLE," +
+                "$COLUMN_DESCRIPTION TEXT)"
+        db.execSQL(createTable)
     }
 
     /**
      * Modifica la tabla de la base de datos
+     *
+     * @param db La base de datos a actualizar.
+     * @param oldVersion La versión anterior de la base de datos.
+     * @param newVersion La nueva versión de la base de datos.
      */
-
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        val dropTableQuery =
-            "DROP TABLE IF EXISTS $TABLE_NAME"
-        db?.execSQL(dropTableQuery)
-        // ya hemos mirado si existia o no, ahora la podemos crear sin que de errores
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_MAP")
         onCreate(db)
     }
 
     /**
-     * Pasado un objeto marcador, lo añade a la base de datos
-     * @param marcador el marcador a añadir
+     * Inserta un nuevo marcador en la base de datos.
+     * @param marcador El marcador a insertar.
      */
-
-    fun insertMarcador(marcador: Marcador){
-        //la abro en modo escritura
+    fun insertar(marcador: Marcador) {
         val db = writableDatabase
-        val values = ContentValues().apply {
-            put(COLUMN_NOMBRE,       marcador.nombre)
-            put(COLUMN_LATITUD,       marcador.latitud)
-            put(COLUMN_LONGITUD, marcador.longitud)
-            //en este caso ID es autonumérico
+        db.beginTransaction()
+        try {
+            val values = ContentValues().apply {
+                put(COLUMN_TITULO, marcador.titulo)
+                put(COLUMN_DESCRIPTION, marcador.descripcion)
+                put(COLUMN_LATITUDE, marcador.latitud)
+                put(COLUMN_LONGITUDE, marcador.longitud)
+            }
+            db.insert(TABLE_MAP, null, values)
+            db.setTransactionSuccessful()
+        } finally {
+            db.endTransaction()
         }
-        //insertamos datos
-        db.insert(TABLE_NAME, null, values)
-        //y ahora a cerrar
-        db.close()
     }
 
     /**
-     * Lee la base de datos y rellena una List de tipo Nota
+     * Devuelve todos los marcadores de la base de datos.
+     * @return Una lista mutable de marcadores.
      */
-
-    fun getAllMarcadores() : List<Marcador> {
-        //creo una lista mutable para poder cambiar cosas
-        val listaMarcadores = mutableListOf<Marcador>()
-
-        //la abro en modo lectura
+    fun getMarcadores(): MutableList<Marcador> {
+        val marcadores = mutableListOf<Marcador>()
         val db = readableDatabase
-
-        val query = "SELECT * FROM $TABLE_NAME"
-
-        //lanza un cursor
-        val cursor = db.rawQuery(query, null)
-
-        //itera mientras que exista otro
-        while (cursor.moveToNext()){
-            val id =            cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
-            val nombre =        cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NOMBRE))
-            val latitud =        cursor.getDouble(cursor.getColumnIndexOrThrow(COLUMN_LATITUD))
-            val longitud =   cursor.getDouble(cursor.getColumnIndexOrThrow(COLUMN_LONGITUD))
-            //creamos un objeto temporal de tipo Marcador
-            val marcador =  Marcador(id, nombre, latitud, longitud)
-            //añadimos la nota
-            listaMarcadores.add(marcador)
+        val cursor = db.query(TABLE_MAP, arrayOf(COLUMN_ID, COLUMN_TITULO, COLUMN_LATITUDE,
+            COLUMN_LONGITUDE, COLUMN_DESCRIPTION), null, null, null, null, null)
+        with(cursor) {
+            while (moveToNext()) {
+                val id = getLong(getColumnIndexOrThrow(COLUMN_ID))
+                val titulo=getString(getColumnIndexOrThrow(COLUMN_TITULO))
+                val latitud=getDouble(getColumnIndexOrThrow(COLUMN_LATITUDE))
+                val longitud=getDouble(getColumnIndexOrThrow(COLUMN_LONGITUDE))
+                val descripcion=getString(getColumnIndexOrThrow(COLUMN_DESCRIPTION))
+                marcadores.add(Marcador(id, latitud,longitud,titulo,descripcion))
+            }
+            close()
         }
-        //cerrar las conexiones
-        cursor.close()
-        db.close()
-
-        return listaMarcadores
+        return marcadores
     }
 
-
-
     /**
-     * Recibe un integer que es su posición en la lista, con esto recuperara los datos del marcador
-     * @return Un marcador en función del integer que es su posicion
-     * @param idMarcador el número de la posición del marcador
+     * Devuelve un marcador específico por su título.
+     * @param titulo El título del marcador a buscar.
+     * @return El marcador encontrado, o null si no existe.
      */
-
-    fun getIdMarcador(idMarcador : Int) : Marcador {
+    fun getMarcadorByTitulo(titulo: String): Marcador? {
         val db = readableDatabase
+        val cursor = db.query(
+            TABLE_MAP,
+            arrayOf(COLUMN_ID, COLUMN_LATITUDE, COLUMN_LONGITUDE, COLUMN_TITULO, COLUMN_DESCRIPTION),
+            "$COLUMN_TITULO = ?",
+            arrayOf(titulo),
+            null, null, null
+        )
 
-        val query = "SELECT * FROM $TABLE_NAME WHERE $COLUMN_ID=$idMarcador"
-
-        //lanza un cursor
-        val cursor = db.rawQuery(query, null)
-        //ve al primer registro que cumpla esa condicion (esperemos que el único)
-        cursor.moveToFirst()
-
-        //leo los datos de la consulta
-        val id =            cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
-        val nombre =        cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NOMBRE))
-        val latitud =        cursor.getDouble(cursor.getColumnIndexOrThrow(COLUMN_LATITUD))
-        val longitud =   cursor.getDouble(cursor.getColumnIndexOrThrow(COLUMN_LONGITUD))
-
-        //cierro conexiones y devuelvo el marcador
-        cursor.close()
-        db.close()
-        return Marcador(id, nombre, latitud, longitud)
-    }
-
-
-    /**
-     * Le pasamos un marcador y actualiza su contenido, tiene una sintaxis especial, no lo hace con un update de sql normal
-     */
-
-    fun updateMarcador(marcador: Marcador) {
-        val db = writableDatabase
-        // creamos los valores a cambiar
-        val values = ContentValues().apply {
-            put(COLUMN_NOMBRE, marcador.nombre)
-            put(COLUMN_LATITUD, marcador.latitud)
-            put(COLUMN_LONGITUD, marcador.longitud)
+        var marcador: Marcador? = null
+        with(cursor) {
+            if (moveToFirst()) {
+                val id = getLong(getColumnIndexOrThrow(COLUMN_ID))
+                val latitud = getDouble(getColumnIndexOrThrow(COLUMN_LATITUDE))
+                val longitud = getDouble(getColumnIndexOrThrow(COLUMN_LONGITUDE))
+                val descripcion = getString(getColumnIndexOrThrow(COLUMN_DESCRIPTION))
+                marcador = Marcador(id, latitud, longitud, titulo, descripcion)
+            }
+            close()
         }
-
-        // parametro con las condiciones a cumplir
-        val whereClausula   = "$COLUMN_ID = ?"
-        //prametro con la lista de elmentos a buscar
-        val whereArgs       = arrayOf(marcador.id.toString())
-
-        //ejecución de esta tabla con esos valores en esa clausula con esas condiciones
-        db.update(TABLE_NAME, values, whereClausula, whereArgs)
-
-        db.close()
+        return marcador
     }
-
-
-    /**
-     * Elimina un marcador dado su id
-     */
-    fun deleteMarcador (idMarcador : Int) {
-        val db = writableDatabase
-
-        val whereClauses    = "$COLUMN_ID = ?"
-        val whereArgs       = arrayOf(idMarcador.toString())
-        //eliminar el objeto
-        db.delete(TABLE_NAME,whereClauses,whereArgs)
-
-        db.close()
-    }
-
 }
